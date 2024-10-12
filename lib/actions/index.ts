656f0757b87a85b2f4bb60fa5b1bd2610903a8da
HarshilTomar20 <harshiltomar20@gmail.com>
2024-10-12 16:24:38 +0530

    feat: issue close

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,9 @@ export async function scrapeAndStoreProduct(productUrl: string) {
  
     try {
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+        //@ts-ignore
+        if(!scrapedProduct) return;
     } catch (error: any) {
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,35 +1,55 @@
-"use server"
+// "use server"
+
+// import axios from "axios";
+// import * as cheerio from 'cheerio';
+
+// export async function scrapeAmazonProduct(url:string) {
+//     if(!url) return;
+    
+//     const username = String(process.env.BRIGHT_DATA_USERNAME || 'brd-customer-hl_7452a0de-zone-priceify');
+//     const password = String(process.env.BRIGHT_DATA_PASSWORD || '3tnoqph7tu69');
+//     const port = 22225;
+//     // Generate a Random session id
+//     const session_id = (1000000 * Math.random()) | 0;
+//     const options = {
+//         auth: {
+//           username: `${username}-session-${session_id}`,
+//           password,
+//         },
+//         host: 'brd.superproxy.io',
+//         port,
+//         rejectUnauthorized: false,
+//       }
+
+//     try {
+//         // Fetch the product page
+//         const response = await axios.get(url, options);
+//         console.log(response);
+//         const $ = cheerio.load(response.data);
+
+//         // Extract the product title
+//         const title = $('#productTitle').text().trim();      
+//         return title; 
+//     } catch (error: any) {
+//         throw new Error(`Failed to scrape product: ${error.message}`)
+//     }
+// }
 
 import axios from "axios";
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
-export async function scrapeAmazonProduct(url:string) {
-    if(!url) return;
-    
-    const username = String(process.env.BRIGHT_DATA_USERNAME);
-    const password = String(process.env.BRIGHT_DATA_PASSWORD);
-    const port = 2225;
-    // Generate a Random session id
-    const session_id = (1000000 * Math.random()) | 0;
-    const options = {
-        auth: {
-            username: `${username}-session-${session_id}`,
-            password,
-        },
-        host: `brd.superproxy.io`,
-        port,
-        rejectUnauthorized: false,
-    }
+export async function scrapeAmazonProduct(url: string) {
+    if (!url) return;
 
     try {
-        const response = await axios.get(url, options);
-        console.log(`BrightData Response is: `,response);
-
+        // Fetch the product page
+        const response = await axios.get(url);
         const $ = cheerio.load(response.data);
 
-        // Extract the Product Title
-        const title = $('#productTitle').text().trim();        
+        // Extract the product title
+        const title = $('#productTitle').text().trim();
+        return title;
     } catch (error: any) {
-        throw new Error(`Failed to scrape product: ${error.message}`)
+        throw new Error(`Failed to scrape product: ${error.message}`);
     }
 }
